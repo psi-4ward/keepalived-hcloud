77b5d4bc12f96c5dbc744c668134638c4af2c37b
Christoph Wiechert <wio@psitrax.de>
2021-12-27 17:45:20 +0100

    Also propagate signals to exporter; exit container if one of the procs terminates

--- rootfs/docker-entrypoint.sh
+++ rootfs/docker-entrypoint.sh
@@ -1,17 +1,45 @@
-#!/bin/bash -e
+#!/bin/bash
+set -eo pipefail
 
 if [ "$1" == "keepalived" ] ; then
   [ ! -e /etc/keepalived/enabled ] && echo 'ENABLED=1' > /etc/keepalived/enabled
 
   touch /tmp/keepalived_notify.log
-
-  touch /tmp/keepalived_notify.log
   (tail -f /tmp/keepalived_notify.log > /dev/stdout)&
+  PIDS="$! $PIDS"
 
-  $DISABLE_EXPORTER || /keepalived-exporter \
-    --ka.pid-path /var/run/keepalived/keepalived.pid \
-    --web.listen-address $EXPORTER_LISTEN_ADDRESS \
-    &
+  # Run keepalived as defined in CMD
+  $@ &
+  PIDS="$! $PIDS"
+
+  if ! $DISABLE_EXPORTER; then
+    /keepalived-exporter \
+      --ka.pid-path /var/run/keepalived/keepalived.pid \
+      --web.listen-address $EXPORTER_LISTEN_ADDRESS \
+      &
+    PIDS="$! $PIDS"
+  fi
+
+  set +e
+
+  function gracefulShutdown {
+    for PID in $PIDS ; do
+      if [ -e /proc/$PID/status ]; then
+        kill -s $1 $PID
+      fi
+    done
+  }
+  trap "gracefulShutdown SIGHUP" SIGHUP
+  trap "gracefulShutdown SIGINT" SIGINT
+  trap "gracefulShutdown SIGTERM" SIGTERM
+
+  wait -n $PIDS
+  E=$?
+
+  gracefulShutdown SIGTERM
+
+  wait $PIDS
+  [ -n "$E" ] && exit $E
+else
+  exec "$@"
 fi
-
-exec "$@"
